1883b4ef9bd7741dffeba324daba6ed27be73749
peter arrenbrecht <peter.arrenbrecht@gmail.com>
2007-08-24 14:41:19 +0200

    [svn] Getting release 1.6 ready.

--- rakefile.rb
+++ rakefile.rb
@@ -1,16 +1,16 @@
 # Make file
 
-VERSION = '1.5.1'
+VERSION = '1.6'
 
 require 'rake/packagetask'
 
 task :docs do
 	require File.join( File.dirname( __FILE__ ), 'lib/rextile' )
 	cd "doc" do 
-        Rextile.new.glob
+	  Rextile.new.processGlob
 	end
 	cd "sample" do
-		Rextile.new.glob
+		Rextile.new.processGlob
 	end
 end
 
@@ -25,6 +25,8 @@ Rake::PackageTask.new( 'rextile', VERSION ) do |p|
 	p.package_files.include "lib/**/*.rb"
 	p.package_files.include "rextile*"
 	p.package_files.include "rakefile.rb"
+	p.package_files.include ".buildpath"
+	p.package_files.include ".project"
 end
 task "pkg/rextile-#{VERSION}" => :build
 
