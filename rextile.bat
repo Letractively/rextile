f392f4ccad099cd699634365a326d46c30242e7e
Peter Arrenbrecht <peter.arrenbrecht@gmail.com>
2009-08-06 09:17:12 +0200

    fix Windows start script and update install instructions

--- rextile.bat
+++ rextile.bat
@@ -3,12 +3,11 @@ if not "%~f0" == "~f0" goto WinNT
 ruby -x "%0" %1 %2 %3 %4 %5 %6 %7 %8 %9
 goto endofruby
 :WinNT
-ruby -x "%0" %*
+ruby -x "%~f0" %*
 exit /b %errorlevel%
 #!/bin/ruby
 
 require 'rubygems'
-Gem.manage_gems
 
 $:.unshift(File.join( File.expand_path( File.dirname( __FILE__ )), 'lib' ))
 
